f8fe8bd2b2bf688238fbd8d77bc3134cfbe54722
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-30 15:41:34 +0100

    connect wallet function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import myEpicNft from './utils/MyEpicNFT.json';
 const CONTRACT_ADDRESS = "0xCb3da68B45F65412603B243B0F4154a24D4cD452";
 
 function App() {
-  
   const [currentAccount, setCurrentAccount] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -41,6 +41,26 @@ function App() {
     }
   }
 
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);; 
+
+      setupEventListener();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const setupEventListener = async () => {
     // Most of this looks the same as our function askContractToMintNft
     try {
@@ -52,9 +72,6 @@ function App() {
         const signer = provider.getSigner();
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-        // THIS IS THE MAGIC SAUCE.
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber())
           alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
